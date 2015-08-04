6f3ff184afa1adc6f9f1f71a6b9c1f9e9745b2ea
CY Chiang <chiang831@gmail.com>
2015-08-05 01:26:03 +0800

    [game] game_setting - Add GameSetting class
    Add GameSetting class to provide different settings for different number
    of players. Add a method to get number of resources in the starting
    resource piles.

---
+++ src/game/game_setting.py
@@ -0,0 +1,10 @@
+import config
+
+class GameSetting(object):
+  """Provide game setting for different number of players."""
+  def __init__(self):
+    pass
+
+  def GetStartResourcesPilesDict(self):
+    """This start resources setting is the same for all number of players."""
+    return config.START_RESOURCES_PILES
