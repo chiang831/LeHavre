2388e9e215867e3e8ff597571a7d66c33a79f383
CY Chiang <chiang831@gmail.com>
2015-08-07 02:58:12 +0800

    [game] player - Add player module
    Add player module. Player class holds name and resource for a player.
    
    BUG=1
    TEST=player_unittest

---
+++ src/game/player.py
@@ -0,0 +1,15 @@
+import resource
+
+class Player(object):
+  def __init__(self, name):
+    self._name = name
+    self._resource = resource.Resource()
+
+  def GetName(self):
+    return self._name
+
+  def GetResource(self):
+    return self._resource
+
+  def AddResource(self, add_resource):
+    self._resource.Add(add_resource)
---
+++ src/game/player_unittest.py
@@ -0,0 +1,27 @@
+import unittest
+
+import player
+import resource
+
+class TestPlayer(unittest.TestCase):
+  def setUp(self):
+    self._name = 'Player1'
+    self._player = player.Player(self._name)
+
+  def testInitPlayer(self):
+    self.assertEqual(self._player.GetName(), self._name)
+
+  def testAddAndGetResource(self):
+    res_dict_1 = dict(franc=1, wood=2)
+    resource_1 = resource.CreateResourceFromDict(res_dict_1)
+    res_dict_2 = dict(franc=1, clay=3)
+    resource_2 = resource.CreateResourceFromDict(res_dict_2)
+    res_dict_sum = dict(franc=2, wood=2, clay=3)
+    resource_sum = resource.CreateResourceFromDict(res_dict_sum)
+
+    self._player.AddResource(resource_1)
+    self._player.AddResource(resource_2)
+    self.assertTrue(resource_sum.Equal(self._player.GetResource()))
+
+if __name__ == '__main__':
+  unittest.main()
