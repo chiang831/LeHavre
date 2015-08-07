c31fe06454a6901c9df8d4d6694f466a4f626ced
CY Chiang <chiang831@gmail.com>
2015-08-07 12:52:23 +0800

    [game] game_flow - Add method to give starting offer
    Add StartingOffer method
    
    BUG=1
    TEST=game_flow_unittest

--- src/game/game_flow.py
+++ src/game/game_flow.py
@@ -3,6 +3,7 @@ import resource
 
 class GameFlow(object):
   def __init__(self, setting):
+    self._setting = setting
     self._resource_pile = resource.CreateResourceFromDict(
         setting.GetStartResourcesPilesDict())
     self._players = list()
@@ -17,6 +18,13 @@ class GameFlow(object):
     for p in self._players:
       if p.GetName() == name:
         return p
+
+  def StartingOffer(self):
+    starting_offer_dict = self._setting.GetLongGameStartingOffer()
+    starting_offer = resource.CreateResourceFromDict(starting_offer_dict)
+    for p in self._players:
+      p.AddResource(starting_offer)
+
 
 def CreateGameFlow(setting):
   return GameFlow(setting)
--- src/game/game_flow_unittest.py
+++ src/game/game_flow_unittest.py
@@ -11,6 +11,11 @@ class TestGameFlow(unittest.TestCase):
     setting = game_setting.GameSetting()
     self._flow = game_flow.CreateGameFlow(setting) 
 
+  def _CreateAndAddPlayer(self, name):
+    p = player.Player(name)
+    self._flow.AddPlayer(p)
+    return p
+
   def testGameFlowStartingResourcePile(self):
     expected_resource_pile = resource.CreateResourceFromDict(
         config.START_RESOURCES_PILES)
@@ -19,9 +24,19 @@ class TestGameFlow(unittest.TestCase):
 
   def testGameFlowAddPlayer(self):
     name = 'Player1'
-    player1 = player.Player(name)
-    self._flow.AddPlayer(player1)
+    self._CreateAndAddPlayer(name)
     self.assertEqual(name, self._flow.GetPlayer(name).GetName())
+
+  def testGameStartingOffer(self):
+    name1 = 'Player1'
+    name2 = 'Player2'
+    p1 = self._CreateAndAddPlayer(name1)
+    p2 = self._CreateAndAddPlayer(name2)
+    self._flow.StartingOffer()
+    expected_resource = resource.CreateResourceFromDict(
+        config.LONG_GAME_STARTING_OFFER)
+    self.assertTrue(p1.GetResource().Equal(expected_resource))
+    self.assertTrue(p2.GetResource().Equal(expected_resource))
 
 if __name__ == '__main__':
   unittest.main()
