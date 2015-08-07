62aa6b7a88b4d2b0dcce91024765560e85881862
CY Chiang <chiang831@gmail.com>
2015-08-07 12:38:02 +0800

    [game] game_flow: Add player to game
    Add AddPlayer and GetPlayer method to GameFlow.
    
    BUG=1
    TEST=game_flow_unittest

--- src/game/game_flow.py
+++ src/game/game_flow.py
@@ -5,9 +5,18 @@ class GameFlow(object):
   def __init__(self, setting):
     self._resource_pile = resource.CreateResourceFromDict(
         setting.GetStartResourcesPilesDict())
+    self._players = list()
 
   def GetResourcePile(self):
     return self._resource_pile
+
+  def AddPlayer(self, new_player):
+    self._players.append(new_player)
+
+  def GetPlayer(self, name):
+    for p in self._players:
+      if p.GetName() == name:
+        return p
 
 def CreateGameFlow(setting):
   return GameFlow(setting)
--- src/game/game_flow_unittest.py
+++ src/game/game_flow_unittest.py
@@ -4,6 +4,7 @@ import config
 import game_flow
 import game_setting
 import resource
+import player
 
 class TestGameFlow(unittest.TestCase):
   def setUp(self):
@@ -15,6 +16,12 @@ class TestGameFlow(unittest.TestCase):
         config.START_RESOURCES_PILES)
     self.assertTrue(
         self._flow.GetResourcePile().Equal(expected_resource_pile))
+
+  def testGameFlowAddPlayer(self):
+    name = 'Player1'
+    player1 = player.Player(name)
+    self._flow.AddPlayer(player1)
+    self.assertEqual(name, self._flow.GetPlayer(name).GetName())
 
 if __name__ == '__main__':
   unittest.main()
