be4f18e6e584ca94826276d8d1883c20e3dc01b1
CY Chiang <chiang831@gmail.com>
2015-08-09 01:38:35 +0800

    [game] game_flow: Fix unittest
    1. Fix unittest so it creates GameSetting with number of players.
    2. Refactor testResourceGenerator.
    
    BUG=1
    TEST=game_flow_unittest

--- src/game/game_flow_unittest.py
+++ src/game/game_flow_unittest.py
@@ -9,9 +9,11 @@ import player
 
 class TestGameFlow(unittest.TestCase):
   def setUp(self):
-    setting = game_setting.GameSetting()
+    self._number_of_players = None
+
+  def _CreateGameFlow(self):
+    setting = game_setting.GameSetting(self._number_of_players)
     self._flow = game_flow.CreateGameFlow(setting) 
-    self._generate_res_list = None
 
   def _CreateAndAddPlayer(self, name):
     p = player.Player(name)
@@ -19,17 +21,23 @@ class TestGameFlow(unittest.TestCase):
     return p
 
   def testGameFlowStartingResourcePile(self):
+    self._number_of_players = 1
+    self._CreateGameFlow()
     expected_resource_pile = resource.CreateResourceFromDict(
         config.START_RESOURCES_PILES)
     self.assertTrue(
         self._flow.GetResourcePile().Equal(expected_resource_pile))
 
   def testGameFlowAddPlayer(self):
+    self._number_of_players = 1
+    self._CreateGameFlow()
     name = 'Player1'
     self._CreateAndAddPlayer(name)
     self.assertEqual(name, self._flow.GetPlayer(name).GetName())
 
   def testGameStartingOffer(self):
+    self._number_of_players = 2
+    self._CreateGameFlow()
     name1 = 'Player1'
     name2 = 'Player2'
     p1 = self._CreateAndAddPlayer(name1)
@@ -40,24 +48,31 @@ class TestGameFlow(unittest.TestCase):
     self.assertTrue(p1.GetResource().Equal(expected_resource))
     self.assertTrue(p2.GetResource().Equal(expected_resource))
 
-  def _SetResourceGenerators(self):
+  def _GetResourceGenerators(self, res_list):
+    res_generator_list = list()
+    for res in res_list:
+      res_generator_list.append(
+          resource_generator.ResourceGenerator(res))
+    return res_generator_list
+
+  def _GetGenerateResourceList(self):
     resource_generator_dicts = config.RESOURCE_GENERATOR_DICTS
-    self._generate_res_list = list()
-    res_gen_list = list()
+    generate_res_list = list()
     for res_gen_dict in resource_generator_dicts:
       res = resource.CreateResourceFromDict(res_gen_dict)
-      self._generate_res_list.append(res)
-      res_gen_list.append(
-          resource_generator.ResourceGenerator(res))
-
-    self._flow.SetResourceGenerators(res_gen_list)
+      generate_res_list.append(res)
+    return generate_res_list
 
   def testResourceGenerator(self):
-    self._SetResourceGenerators()
+    self._number_of_players = 1
+    self._CreateGameFlow()
+    generate_res_list = self._GetGenerateResourceList()
+    res_generator_list = self._GetResourceGenerators(generate_res_list)
+    self._flow.SetResourceGenerators(res_generator_list)
 
     expected_res_pile = self._flow.GetResourcePile().Copy()
-    for turn in xrange(len(self._generate_res_list)):
-      expected_res_pile.Add(self._generate_res_list[turn])
+    for turn in xrange(len(generate_res_list)):
+      expected_res_pile.Add(generate_res_list[turn])
 
       self._flow.GenerateResource()
       self.assertTrue(
@@ -66,6 +81,8 @@ class TestGameFlow(unittest.TestCase):
       self._flow.NextTurn()
 
   def testTakeResource(self):
+    self._number_of_players = 1
+    self._CreateGameFlow()
     res_pile = resource.Resource(franc=1, clay=2)
     name = 'Player1'
     p1 = self._CreateAndAddPlayer(name)
@@ -75,6 +92,8 @@ class TestGameFlow(unittest.TestCase):
         resource.Resource(franc=1)))
 
   def testGetCurrentPlayer(self):
+    self._number_of_players = 2
+    self._CreateGameFlow()
     name1 = 'Player1'
     name2 = 'Player2'
     p1 = self._CreateAndAddPlayer(name1)
