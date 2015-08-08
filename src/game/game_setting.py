7d34aeda8e0d3856684c97da89dadf0434365256
CY Chiang <chiang831@gmail.com>
2015-08-09 01:35:45 +0800

    [game] game_setting - Add setting for end of round
    Add setting for end of round under each number of players. Now
    GameSetting needs an argument for number of players.
    Unittest of game_flow will be fixed in the next commit.
    
    BUG=1
    TEST=game_setting_unittest

--- src/game/game_setting.py
+++ src/game/game_setting.py
@@ -2,8 +2,8 @@ import config
 
 class GameSetting(object):
   """Provide game setting for different number of players."""
-  def __init__(self):
-    pass
+  def __init__(self, number_of_players):
+    self._number_of_players = number_of_players
 
   def GetStartResourcesPilesDict(self):
     """This start resources setting is the same for all number of players."""
@@ -11,3 +11,6 @@ class GameSetting(object):
 
   def GetLongGameStartingOffer(self):
     return config.LONG_GAME_STARTING_OFFER
+
+  def GetEndOfRound(self, round_index):
+    return config.END_OF_ROUND_DICT[self._number_of_players][round_index]
--- src/game/game_setting_unittest.py
+++ src/game/game_setting_unittest.py
@@ -5,17 +5,56 @@ import config
 import game_setting
 
 class TestGameSetting(unittest.TestCase):
+  def setUp(self):
+    self._number_of_players = 1
+
   def testGameSettingStartResourcePilesDict(self):
-    game_setting_obj = game_setting.GameSetting()
-    start_resources = game_setting_obj.GetStartResourcesPilesDict()
+    self._CreateGameSettingObject()
+    start_resources = self._game_setting_obj.GetStartResourcesPilesDict()
     expected_resources = config.START_RESOURCES_PILES
     self.assertEqual(start_resources, expected_resources)
 
   def testGameSettingLongGameStartingOffer(self):
-    game_setting_obj = game_setting.GameSetting()
-    starting_offer = game_setting_obj.GetLongGameStartingOffer()
+    self._CreateGameSettingObject()
+    starting_offer = self._game_setting_obj.GetLongGameStartingOffer()
     expected_resources = config.LONG_GAME_STARTING_OFFER
     self.assertEqual(starting_offer, expected_resources)
+
+  def _TestEndOfRound(self):
+    self._CreateGameSettingObject()
+    number_of_rounds = len(config.END_OF_ROUND_DICT[self._number_of_players])
+    for r in xrange(number_of_rounds):
+      self._TestEndOfRoundOneRound(r) 
+
+  def _TestEndOfRoundOneRound(self, round_index):
+    round_ending_obj = self._game_setting_obj.GetEndOfRound(round_index)
+    self.assertEqual(
+        round_ending_obj,
+        config.END_OF_ROUND_DICT[self._number_of_players][round_index])
+
+  def _CreateGameSettingObject(self):
+    self._game_setting_obj = game_setting.GameSetting(self._number_of_players)
+    
+  def testEndOfRoundOnePlayer(self):
+    self._number_of_players = 1
+    self._TestEndOfRound()
+
+  def testEndOfRoundTwoPlayer(self):
+    self._number_of_players = 2
+    self._TestEndOfRound()
+
+  def testEndOfRoundThreePlayer(self):
+    self._number_of_players = 3
+    self._TestEndOfRound()
+
+  def testEndOfRoundFourPlayer(self):
+    self._number_of_players = 4
+    self._TestEndOfRound()
+
+  def testEndOfRoundFivePlayer(self):
+    self._number_of_players = 5
+    self._TestEndOfRound()
+
 
 if __name__ == '__main__':
   unittest.main()
