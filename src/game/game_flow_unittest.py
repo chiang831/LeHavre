a47578271912ac7130c96ebe64f6d823ffd90b1e
CY Chiang <chiang831@gmail.com>
2015-08-08 22:10:41 +0800

    [game] game_flow - Refactor game_flow unittest
    Move setting resource generator list into a method
    
    BUG=None
    TEST=game_flow_unittest

--- src/game/game_flow_unittest.py
+++ src/game/game_flow_unittest.py
@@ -11,6 +11,7 @@ class TestGameFlow(unittest.TestCase):
   def setUp(self):
     setting = game_setting.GameSetting()
     self._flow = game_flow.CreateGameFlow(setting) 
+    self._generate_res_list = None
 
   def _CreateAndAddPlayer(self, name):
     p = player.Player(name)
@@ -39,20 +40,24 @@ class TestGameFlow(unittest.TestCase):
     self.assertTrue(p1.GetResource().Equal(expected_resource))
     self.assertTrue(p2.GetResource().Equal(expected_resource))
 
-  def testResourceGenerator(self):
+  def _SetResourceGenerators(self):
     resource_generator_dicts = config.RESOURCE_GENERATOR_DICTS
-    res_list = list()
+    self._generate_res_list = list()
     res_gen_list = list()
     for res_gen_dict in resource_generator_dicts:
       res = resource.CreateResourceFromDict(res_gen_dict)
-      res_list.append(res)
+      self._generate_res_list.append(res)
       res_gen_list.append(
           resource_generator.ResourceGenerator(res))
 
     self._flow.SetResourceGenerators(res_gen_list)
+
+  def testResourceGenerator(self):
+    self._SetResourceGenerators()
+
     expected_res_pile = self._flow.GetResourcePile().Copy()
-    for turn in xrange(config.NUMBER_OF_TURNS):
-      expected_res_pile.Add(res_list[turn])
+    for turn in xrange(len(self._generate_res_list)):
+      expected_res_pile.Add(self._generate_res_list[turn])
 
       self._flow.GenerateResource()
       self.assertTrue(
