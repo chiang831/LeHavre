b23d23ae2d4de23d636691f5ba3db2bb91e3ec3e
CY Chiang <chiang831@gmail.com>
2015-08-09 23:17:21 +0800

    [game] food_checker - Add food_checker module to checkpaying food
    FoodChecker handles the logic of checking if a resource is valid for
    paying food.
    It checks if resource is not enough, just make, too much, or too much
    but needed.
    
    BUG=1
    TEST=food_checker_unittest

---
+++ src/game/food_checker.py
@@ -0,0 +1,47 @@
+import resource
+
+class FoodTooMuchError(Exception):
+  pass
+
+
+class NotFoodError(Exception):
+  pass
+
+
+class FoodChecker(object):
+  def __init__(self, target):
+    self._target = target
+    self._res = None
+    self._check_result = None
+
+  def Check(self, res):
+    self._res = res
+    self._CheckNonFood()
+    return self._CheckFoodNumber()
+
+  def _CheckNonFood(self):
+    resource_element_dict = self._res.GetNonZeroResourceElementDict()
+    for element in resource_element_dict.values():
+      if not element.GetUnitFoodValue():
+        raise NotFoodError('Not a food: %s' % element.name)
+
+  def _CheckFoodNumber(self):
+    food = self._res.GetFoodValue()
+    
+    if food < self._target:
+      return False
+
+    elif food == self._target:
+      return True
+
+    # Resource contains too much food.
+    # Check if we can put back some resource element.
+    else:
+      resource_number_dict = self._res.GetNonZeroResourceNumberDict()
+      for key, value in resource_number_dict.iteritems():
+        test_dict = dict(resource_number_dict)
+        test_dict[key] = value - 1
+        test_res = resource.CreateResourceFromDict(test_dict)
+        if test_res.GetFoodValue() >= self._target:
+          raise FoodTooMuchError('Too much resource')
+      return True
---
+++ src/game/food_checker_unittest.py
@@ -0,0 +1,48 @@
+import unittest
+
+import food_checker
+import resource
+
+class TestFoodChecker(unittest.TestCase):
+  def setUp(self):
+    # Initialize the targeted food to 2.
+    self._target_food = 2
+    self._food_checker = food_checker.FoodChecker(
+        self._target_food)
+
+  def testCheckEnough(self):
+    picked_res = resource.Resource(fish=2)
+    self.assertTrue(self._food_checker.Check(picked_res))
+
+  def testCheckEnough(self):
+    picked_res = resource.Resource(franc=1, fish=1)
+    self.assertTrue(self._food_checker.Check(picked_res))
+
+  def testCheckNotEnough(self):
+    picked_res = resource.Resource(fish=1)
+    self.assertFalse(self._food_checker.Check(picked_res))
+
+  def testCheckTooMuch(self):
+    picked_res = resource.Resource(franc=1, fish=2)
+    with self.assertRaises(food_checker.FoodTooMuchError):
+      self._food_checker.Check(picked_res)
+
+  def testCheckTooMuchButNeededMeal(self):
+    picked_res = resource.Resource(meal=1)
+    self.assertTrue(self._food_checker.Check(picked_res))
+
+  def testCheckTooMuchButNeededBread(self):
+    picked_res = resource.Resource(bread=1)
+    self._target_food = 1
+    self._food_checker = food_checker.FoodChecker(
+        self._target_food)
+    self.assertTrue(self._food_checker.Check(picked_res))
+
+  def testCheckNonValid(self):
+    picked_res = resource.Resource(franc=1, fish=1, clay=1)
+    with self.assertRaises(food_checker.NotFoodError):
+      self._food_checker.Check(picked_res)
+
+
+if __name__ == '__main__':
+  unittest.main()
