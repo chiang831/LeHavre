fc51cbc601e8ff58454184dc069fbe8881dd69b2
CY Chiang <chiang831@gmail.com>
2015-09-13 01:42:27 +0800

    [game] enter_building_handler - Add module to handle player entering building

---
+++ src/game/enter_building_handler.py
@@ -0,0 +1,53 @@
+"""Module to handle entering building"""
+
+import entry_fee_checker
+
+class NotEnoughEntryFeeError(Exception):
+  pass
+
+class NotPickedEnoughEntryFeeError(Exception):
+  pass
+
+class EnterBuildingHandler(object):
+  def __init__(self, building_obj, player_obj, picker_obj):
+    self._building = building_obj
+    self._player = player_obj
+    self._picker = picker_obj
+    self._checker = None
+    self._picked_res = None
+    self._CreateChecker()
+    self._CheckCanEnter()
+
+  def _CreateChecker(self):
+    self._checker = entry_fee_checker.EntryFeeChecker(self._building.GetFee())
+
+  def _CheckCanEnter(self):
+    all_picked_res = self._player.GetResource().Copy()
+    can_enter = False
+    try:
+      can_enter = self._checker.Check(all_picked_res)
+    # Too much is ok since we just check that player has enough resource to
+    # pay entry fee.
+    except entry_fee_checker.TooMuchError:
+      can_enter = True
+    if not can_enter:
+      raise NotEnoughEntryFeeError('Not enough resource for entry fee.')
+
+  def EnterBuilding(self):
+    self._picked_res = self._picker.GetPicked()
+    self._CheckPickedEnough()
+    self._PayEntryFee()
+    self._OccupyBuilding()
+
+  def _CheckPickedEnough(self):
+    can_enter = self._checker.Check(self._picked_res)
+    if not can_enter:
+      raise NotPickedEnoughEntryFeeError(
+          'Not picked enough resource for entry fee')
+
+  def _PayEntryFee(self):
+    self._player.SubtractResource(self._picked_res)
+
+  def _OccupyBuilding(self):
+    self._player.SetWorkerPlace(self._building.GetName())
+    self._building.SetCurrentWorker(self._player.GetName())
---
+++ src/game/enter_building_handler_unittest.py
@@ -0,0 +1,71 @@
+"""Unittest for enter_building_handler."""
+import unittest
+
+import building
+import enter_building_handler
+import entry_fee
+import player
+import resource
+import resource_picker
+
+class TestEnterBuildingHandler(unittest.TestCase):
+  def setUp(self):
+    self._building = None
+    self._player = None
+    self._picker = None
+    self._fee = None
+    self._handler = None
+
+  def _CreateBuilding(self):
+    self._building = building.CreateBuilding(
+        'building1', resource.Resource(wood=1, clay=1),
+        10, self._fee)
+
+  def _CreatePlayer(self):
+    self._player = player.Player('Player1')
+    self._player.AddResource(resource.Resource(fish=2))
+
+  def _CreatePicker(self):
+    self._picker = resource_picker.CreateResourcePickerForEntryFee(
+        self._player.GetResource(), self._fee)
+
+  def _CreateHandler(self):
+    self._handler = enter_building_handler.EnterBuildingHandler(
+        building_obj=self._building,
+        player_obj=self._player,
+        picker_obj=self._picker,
+        )
+
+  def testEnterBuilding(self):
+    self._fee = entry_fee.EntryFee(franc=0, food=1)
+    self._CreateBuilding()
+    self._CreatePlayer()
+    self._CreatePicker()
+    self._CreateHandler()
+    self._picker.Pick(fish=1)
+    self._handler.EnterBuilding()
+    self.assertEqual(self._player.GetWorkerPlace(), self._building.GetName())
+    self.assertTrue(self._player.GetResource().Equal(resource.Resource(fish=1)))
+    self.assertTrue(self._building.IsOccupied())
+    self.assertEqual(self._building.GetCurrentWorker(), self._player.GetName())
+
+  def testCanNotEnter(self):
+    self._fee = entry_fee.EntryFee(franc=0, food=3)
+    self._CreateBuilding()
+    self._CreatePlayer()
+    self._CreatePicker()
+    with self.assertRaises(enter_building_handler.NotEnoughEntryFeeError):
+      self._CreateHandler()
+
+  def testNotPickedEnough(self):
+    self._fee = entry_fee.EntryFee(franc=0, food=2)
+    self._CreateBuilding()
+    self._CreatePlayer()
+    self._CreatePicker()
+    self._CreateHandler()
+    self._picker.Pick(fish=1)
+    with self.assertRaises(enter_building_handler.NotPickedEnoughEntryFeeError):
+      self._handler.EnterBuilding()
+
+if __name__ == '__main__':
+  unittest.main()
